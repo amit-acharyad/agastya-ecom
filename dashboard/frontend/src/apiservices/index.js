4161c7450c8d7a8bbe62aa429c826f785b2862fc
GunarajKhatri <khatriroshan54@gmail.com>
2023-12-27 10:53:56 +0545

    basic folder structure added

---
+++ dashboard/frontend/src/apiservices/index.js
@@ -0,0 +1,6 @@
+import { deleteRequest, getRequest, postRequest, putRequest } from "./fetch";
+
+// export const getProducts = async () => {
+//     const res = await getRequest("app/products", "jwt");
+//     return res.data;
+//   };
